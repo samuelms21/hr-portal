ccfb49a47357843e8a323747f22cffec48b19f38
Daniel Wiratman <74503671+danielwiratman@users.noreply.github.com>
2023-04-24 09:41:04 +0700

    working on sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ import { StaffPermitHistoryComponent } from './staff/staff-permit-history/staff-
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'admin',
     pathMatch: 'full',
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputTextModule } from 'primeng/inputtext';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,16 +30,44 @@ import { StaffLeaveFormComponent } from './staff/staff-leave-form/staff-leave-fo
 import { StaffLeaveHistoryComponent } from './staff/staff-leave-history/staff-leave-history.component';
 import { StaffPermitFormComponent } from './staff/staff-permit-form/staff-permit-form.component';
 import { StaffPermitHistoryComponent } from './staff/staff-permit-history/staff-permit-history.component';
+import { NgIconsModule } from '@ng-icons/core';
+
+import { matHome, matPeople, matSettings } from '@ng-icons/material-icons/baseline';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, AdminComponent, AdminDashboardComponent, EmployeeDataComponent, EmployeeDetailComponent, AddEditEmployeeComponent, HeaderComponent, SidebarComponent, StaffComponent, EmployeeLeaveConfirmationComponent, EmployeeLeaveAllComponent, EmployeeLeaveReportComponent, SettingsHolidayComponent, SettingsLeaveLimitComponent, SettingsPermitCategoryComponent, StaffDashboardComponent, StaffInfoComponent, StaffLeaveFormComponent, StaffLeaveHistoryComponent, StaffPermitFormComponent, StaffPermitHistoryComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    AdminDashboardComponent,
+    EmployeeDataComponent,
+    EmployeeDetailComponent,
+    AddEditEmployeeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    StaffComponent,
+    EmployeeLeaveConfirmationComponent,
+    EmployeeLeaveAllComponent,
+    EmployeeLeaveReportComponent,
+    SettingsHolidayComponent,
+    SettingsLeaveLimitComponent,
+    SettingsPermitCategoryComponent,
+    StaffDashboardComponent,
+    StaffInfoComponent,
+    StaffLeaveFormComponent,
+    StaffLeaveHistoryComponent,
+    StaffPermitFormComponent,
+    StaffPermitHistoryComponent,
+  ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule,
-    ButtonModule,
     BrowserAnimationsModule,
-    InputTextModule,
+    AppRoutingModule,
     FormsModule,
+    InputTextModule,
+    ButtonModule,
+    NgIconsModule.withIcons({ matHome, matPeople, matSettings }),
   ],
   providers: [],
   bootstrap: [AppComponent],
